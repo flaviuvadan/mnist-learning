4bb5b967df8452011367f4a18ba65f840e60b4b7
FlaviuVadan <flaviuvadan@gmail.com>
2019-04-21 16:02:14 -0600

    Add vectorized_result function

--- mnist-practice/network/functions.py
+++ mnist-practice/network/functions.py
@@ -15,3 +15,11 @@ class Functions:
     def sigmoid_prime(cls, x):
         """ Derivative of sigmoid function """
         return cls.sigmoid(x) * (1 - cls.sigmoid(x))
+
+    @classmethod
+    def vectorized_result(cls, j):
+        """ Get a 10D unit vector with a 1.0 in the j'th position and zeroes elsewhere. Typical use case is to convert
+        the activation layer of a net to a single output """
+        e = numpy.zeros((10, 1))
+        e[j] = 1.0
+        return e
