a57c90987fc2681c47aacf82f7afaa6f678d66c9
FlaviuVadan <flaviuvadan@gmail.com>
2019-04-25 06:09:01 -0600

    Add conv layer simple blueprint

---
+++ mnist-practice/network/conv_layer_blueprint.py
@@ -0,0 +1,9 @@
+""" Convolutional layer blueprint """
+
+
+class ConvLayerBlueprint:
+    """ Conv layer blueprint """
+
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+        """ Set input """
+        raise NotImplementedError
