1bce72661d3f6028e7bf155eae3de5a19e04b3e0
FlaviuVadan <flaviuvadan@gmail.com>
2019-04-25 05:38:02 -0600

    Adjust params again

--- mnist-practice/learn.py
+++ mnist-practice/learn.py
@@ -10,11 +10,12 @@ def start():
     training_data, validation_data, test_data = load.load_data()
 
     print("{} Training NetworkTest1 {}".format(p, p))
-    net = network_test_1.NetworkTest1([784, 50, 10])
+    net = network_test_1.NetworkTest1([784, 30, 10])
     net.stochastic_gradient_descent(training_data, 30, 10, 3.0, test_data=test_data)
 
+    training_data, validation_data, test_data = load.load_data()
     print("{} Training NetworkTest2 {}".format(p, p))
-    net2 = network_test_2.NetworkTest2([784, 50, 10])
+    net2 = network_test_2.NetworkTest2([784, 30, 10])
     net2.stochastic_gradient_descent(training_data, 30, 10, 0.5, 5,
                                      evaluation_data=test_data,
                                      monitor_evaluation_accuracy=True,
