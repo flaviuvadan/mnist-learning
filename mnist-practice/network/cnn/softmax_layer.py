e8627a6baa33d8107b5db1fc66b510edaeb5986e
FlaviuVadan <flaviuvadan@gmail.com>
2019-04-25 18:45:22 -0600

    Add softmax layer skeleton

---
+++ mnist-practice/network/cnn/softmax_layer.py
@@ -0,0 +1,25 @@
+""" Softmax layer """
+
+from .conv_layer_blueprint import ConvLayerBlueprint
+
+
+class SoftmaxLayer(ConvLayerBlueprint):
+    """ Softmax layer """
+
+    def __init__(self):
+        pass
+
+    def set_inpt(self, inpt, inpt_dropout, mini_batch_size):
+        pass
+
+    def set_weights(self):
+        pass
+
+    def set_biases(self):
+        pass
+
+    def accuracy(self):
+        pass
+
+    def cost(self):
+        pass
