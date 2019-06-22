a4f16110fb3d36216485ffe0f86fb7f69a432c3d
FlaviuVadan <flaviuvadan@gmail.com>
2019-06-22 14:08:51 -0600

    Update plot

--- mnist-practice/plot/plot.py
+++ mnist-practice/plot/plot.py
@@ -6,16 +6,20 @@ from matplotlib import pyplot, lines
 class Plot:
     """ Plot class """
 
-    def __init__(self, x_values, y_values, title, filename):
+    def __init__(self, x_values, x_label, y_values, y_label, title, filename):
         """
         Initialize a plot class
         :param x_values: [x1, x2, ..., xn]
+        :param x_label: string - label of the X axis
         :param y_values: [y1, y2, ..., yn]
+        :param y_label: string - label of the Y axis
         :param title: title of the plot
         :param filename: name of the file where the chart is saved
         """
         self.x_values = x_values
+        self.x_label = x_label
         self.y_values = y_values
+        self.y_label = y_label
         self.title = title
         self.filename = filename
 
@@ -24,6 +28,9 @@ class Plot:
         fig, ax = pyplot.subplots()
         line = lines.Line2D(self.x_values, self.y_values)
         ax.add_line(line)
-        ax.axis([0, len(self.x_values), 0, max(self.y_values)])
+        ax.set_title(self.title)
+        ax.axis([0, len(self.x_values) + 1, 0, max(self.y_values) + 0.1])
+        ax.set_xlabel(self.x_label)
+        ax.set_ylabel(self.y_label)
         pyplot.savefig(self.filename)
         fig.clear()
